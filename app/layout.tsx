a9b09f5af157278e76869ad357a8b43a32b4a2ad
Kerem Aydemir <keremaydemir59@gmail.com>
2023-09-12 22:01:59 +0300

    feat: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,24 @@ export default function RootLayout({
         <meta name="description" content="Personal website of Kerem Aydemir" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest"></link>
       </head>
 
       <body
